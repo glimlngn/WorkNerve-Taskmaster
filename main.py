23edde84d5d7e81ada9ec62cadfffecdf0e103b5
Gab Limlingan <glimlingan@gmail.com>
2026-08-11 01:46:38 +0800

    feat: single auto task entry

--- main.py
+++ main.py
@@ -11,33 +11,91 @@ PROFILE_DIR = os.path.abspath("./selenium_profile")
 
 # Setup Chromium browser and go to worknerve.techoneglobal.com
 WORKNERVE_URL = 'https://worknerve.techoneglobal.com/login'
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument(f"--user-data-dir={PROFILE_DIR}")
-chrome_options.add_argument("--profile-directory=Default")
-chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-chrome_options.add_experimental_option('detach', True)
-driver = webdriver.Chrome(options=chrome_options)
+options = webdriver.ChromeOptions()
+options.add_argument(f"--user-data-dir={PROFILE_DIR}")
+options.add_argument("--profile-directory=Default")
+options.add_argument("--disable-blink-features=AutomationControlled")
+driver = webdriver.Chrome(options=options)
 actions = ActionChains(driver)
-
 driver.get(WORKNERVE_URL)
+
+# Assuming auth retented, go to 'My Tasks' and hover on the '+ Add Task' button.
 actions.send_keys(Keys.TAB * 3).send_keys(Keys.ENTER).perform()
-time.sleep(5)
+time.sleep(3)
 input_element = driver.find_element(By.CSS_SELECTOR, "[aria-label='My Tasks']")
 input_element.send_keys(Keys.ENTER)
-actions.send_keys(Keys.TAB * 13).send_keys(Keys.ENTER).perform()
+time.sleep(1)
+actions.send_keys(Keys.TAB * 13).perform()
 
-input('BREAKPOINT: enter anything to close the browser')
+# input('BREAKPOINT: press ENTER to continue.')
 
-# # Get task list from Excel file
-# path = 'worknerve_tasks_aug3_aug4.xlsx'
-# task_list = pd.read_excel(path)
-# input_element = driver.find_element(By.ID, '_r_1f_')
-# input_element.send_keys('Hello World')
+# Get task list from Excel file
+path = 'test_file.xlsx'
+task_list = pd.read_excel(path)
+
+# Press 'Add Task'
+actions.send_keys(Keys.ENTER).perform()
+actions.send_keys(Keys.TAB * 5).perform()
+
+# TODO: Loop through all tasks in the Excel file
+task_index = 8
+# Input Task Data
+# Service Line
+actions.send_keys(task_list.iloc[task_index]['service_line']).perform()
+time.sleep(0.3)
+actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+# Category
+actions.send_keys(task_list.iloc[task_index]['category']).perform()
+time.sleep(0.3)
+actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+# Sub-category
+actions.send_keys(task_list.iloc[task_index]['subcategory']).perform()
+time.sleep(0.3)
+actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+# Project/Program/Managed Service (for manual input)
+with_oppo = task_list.iloc[task_index]['with_oppo']
+if(with_oppo == 'Yes'):
+    actions.send_keys('').perform()
+    time.sleep(0.3)
+    actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+elif(with_oppo == 'Sometimes'): 
+    actions.send_keys(Keys.TAB).perform()
+elif(with_oppo == 'No'):
+    pass
+
+# Task Title
+actions.send_keys(task_list.iloc[task_index]['task_title']).perform()
+time.sleep(0.3)
+actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+# Description
+actions.send_keys(task_list.iloc[task_index]['description']).perform()
+time.sleep(0.3)
+actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+# TODO: automate Priority selection
+actions.send_keys(Keys.TAB).perform()
+# actions.send_keys(task_list.iloc[task_index]['priority']).perform()
+# time.sleep(0.3)
+# actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+# Estimated Hours
+actions.send_keys(task_list.iloc[task_index]['estimated_hours']).perform()
+time.sleep(0.3)
+actions.send_keys(Keys.ENTER).perform()
+actions.send_keys(Keys.ENTER, Keys.TAB, Keys.TAB).perform()
+
+# Add Task to WorkNerve
+actions.send_keys(Keys.ENTER).perform()
+time.sleep(3)
+
+# Press '+ Add Task' and move to Service Line
+actions.send_keys(Keys.ENTER).perform()
+actions.send_keys(Keys.TAB * 5).perform()
+
+input('BREAKPOINT: press ENTER to continue.')
 
 driver.quit()
-
-# def add_task(task):
-#     return
-
-# for index, task in task_list.iterrows():
-#     add_task(task)
