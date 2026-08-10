4a0274dba72658889a87aa5efb46531aad3d8f01
Gab Limlingan <glimlingan@gmail.com>
2026-08-11 02:28:59 +0800

    feat: bulk auto task entry

--- main.py
+++ main.py
@@ -21,84 +21,79 @@ driver.get(WORKNERVE_URL)
 
 # Assuming auth retented, go to 'My Tasks' and hover on the '+ Add Task' button.
 actions.send_keys(Keys.TAB * 3).send_keys(Keys.ENTER).perform()
-time.sleep(3)
+time.sleep(5)
 input_element = driver.find_element(By.CSS_SELECTOR, "[aria-label='My Tasks']")
 input_element.send_keys(Keys.ENTER)
-time.sleep(1)
+time.sleep(3)
 actions.send_keys(Keys.TAB * 13).perform()
 
 # Get task list from Excel file
 path = 'test_file.xlsx'
 task_list = pd.read_excel(path)
 
-# Press 'Add Task'
-actions.send_keys(Keys.ENTER).perform()
-actions.send_keys(Keys.TAB * 5).perform()
-
-# TODO: Loop through all tasks in the Excel file
-task_index = 1
 # Input Task Data
-# Service Line
-actions.send_keys(task_list.iloc[task_index]['service_line']).perform()
-time.sleep(0.5)
-actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+for index, task in task_list.iterrows(): 
+    # Press '+ Add Task' and move to Service Line
+    actions.send_keys(Keys.ENTER).perform()
+    actions.send_keys(Keys.TAB * 5).perform()
 
-# Category
-actions.send_keys(task_list.iloc[task_index]['category']).perform()
-time.sleep(0.5)
-actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
-
-# Sub-category
-actions.send_keys(task_list.iloc[task_index]['subcategory']).perform()
-time.sleep(0.5)
-actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
-
-# Project/Program/Managed Service (for manual input)
-with_oppo = task_list.iloc[task_index]['with_oppo']
-if(with_oppo == 'Yes'):
-    actions.send_keys(' ').perform()
+    # Service Line
+    actions.send_keys(task['service_line']).perform()
     time.sleep(0.5)
     actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
-elif(with_oppo == 'Sometimes'): 
+
+    # Category
+    actions.send_keys(task['category']).perform()
+    time.sleep(0.5)
+    actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+    # Sub-category
+    actions.send_keys(task['subcategory']).perform()
+    time.sleep(0.5)
+    actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+    # Project/Program/Managed Service (for manual input)
+    with_oppo = task['with_oppo']
+    if(with_oppo == 'Yes'):
+        actions.send_keys(' ').perform()
+        time.sleep(0.5)
+        actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+    elif(with_oppo == 'Sometimes'): 
+        actions.send_keys(Keys.TAB).perform()
+    elif(with_oppo == 'No'):
+        pass
+
+    # Task Title
+    actions.send_keys(task['task_title']).perform()
+    time.sleep(0.5)
+    actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+    # Description
+    actions.send_keys(task['description']).perform()
+    time.sleep(0.5)
+    actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+
+    # Priority
+    priority = task['priority']
+    if(priority == 'Low'):
+        actions.send_keys(Keys.ENTER, Keys.ARROW_UP, Keys.ENTER).perform()
+    elif(priority == 'High'):
+        actions.send_keys(Keys.ENTER, Keys.ARROW_DOWN, Keys.ENTER).perform()
+    elif(priority == 'Urgent'): 
+        actions.send_keys(Keys.ENTER, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER).perform()
+        pass
     actions.send_keys(Keys.TAB).perform()
-elif(with_oppo == 'No'):
-    pass
 
-# Task Title
-actions.send_keys(task_list.iloc[task_index]['task_title']).perform()
-time.sleep(0.5)
-actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+    # Estimated Hours
+    actions.send_keys(task['estimated_hours']).perform()
+    time.sleep(0.5)
+    actions.send_keys(Keys.ENTER).perform()
+    actions.send_keys(Keys.ENTER, Keys.TAB, Keys.TAB).perform()
 
-# Description
-actions.send_keys(task_list.iloc[task_index]['description']).perform()
-time.sleep(0.5)
-actions.send_keys(Keys.ARROW_DOWN, Keys.ENTER, Keys.TAB).perform()
+    # Add Task to WorkNerve
+    actions.send_keys(Keys.ENTER).perform()
+    time.sleep(3)
 
-# Priority
-priority = task_list.iloc[task_index]['priority']
-if(priority == 'Low'):
-    actions.send_keys(Keys.ENTER, Keys.ARROW_UP, Keys.ENTER).perform()
-elif(priority == 'High'):
-    actions.send_keys(Keys.ENTER, Keys.ARROW_DOWN, Keys.ENTER).perform()
-elif(priority == 'Urgent'): 
-    actions.send_keys(Keys.ENTER, Keys.ARROW_DOWN, Keys.ARROW_DOWN, Keys.ENTER).perform()
-    pass
-actions.send_keys(Keys.TAB).perform()
-
-# Estimated Hours
-actions.send_keys(task_list.iloc[task_index]['estimated_hours']).perform()
-time.sleep(0.5)
-actions.send_keys(Keys.ENTER).perform()
-actions.send_keys(Keys.ENTER, Keys.TAB, Keys.TAB).perform()
-
-# Add Task to WorkNerve
-actions.send_keys(Keys.ENTER).perform()
-time.sleep(3)
-
-# Press '+ Add Task' and move to Service Line
-actions.send_keys(Keys.ENTER).perform()
-actions.send_keys(Keys.TAB * 5).perform()
-
-input('BREAKPOINT: press ENTER to continue.')
+input('BREAKPOINT: press ENTER to continue. ')
 
 driver.quit()
